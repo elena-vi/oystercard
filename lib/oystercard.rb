e2bfa40a1df2a2eb22c766befb5d57c79024d592
Elena <elena.vilimaite@gmail.com>
2016-05-30 13:43:06 +0100

    touching in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,15 @@
 class Oystercard
 
 	attr_reader :balance
-  LIMIT = 90
+	LIMIT = 90
 
 	def initialize
 		@balance = 0
+		@in_journey = false
 	end
 
 	def top_up(amount)
-    fail "Error: cannot have balance greater than £#{LIMIT}" if @balance+amount > LIMIT
+		fail "Error: cannot have balance greater than £#{LIMIT}" if @balance+amount > LIMIT
 		@balance += amount
 		self
 	end
@@ -17,4 +18,15 @@ class Oystercard
 		@balance -= fare
 	end
 
+	def touch_in
+		@in_journey = true
+	end
+
+	def touch_out
+		@in_journey = false
+	end
+
+	def in_journey?
+		@in_journey
+	end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,6 +4,8 @@ describe Oystercard do
 
 	subject(:card) {described_class.new}
 
+	it {is_expected.to respond_to(:in_journey?)}
+
 	describe "#balance" do
 		it {is_expected.to respond_to(:balance)}
 
@@ -31,10 +33,20 @@ describe Oystercard do
 		it 'tops up with £10' do
 			expect{ card.deduct 10 }.to change{ card.balance }.by -10
 		end
+	end
 
-		# it "has a £#{Oystercard::LIMIT} upper limit for balance" do
-		#   max_balance = Oystercard::LIMIT
-		#   expect{ card.deduct(max_balance+1) }.to raise_error "Error: cannot have balance greater than £#{max_balance}"
-		# end
+	describe "#touch_in" do
+		it {is_expected.to respond_to(:touch_in)}
+		it 'touch in and check status' do
+			expect(card.touch_in).to eq true
+			expect(card.in_journey?).to eq true
+		end
+	end
+	describe "#touch_out" do
+		it {is_expected.to respond_to(:touch_out)}
+		it 'touch out and check status' do
+			expect(card.touch_out).to eq false
+			expect(card.in_journey?).to eq false
+		end
 	end
 end
