bbcb9d8e27cac2da3b77cc3ada41799655fab992
awye765 <awye765@googlemail.com>
2016-05-30 12:05:00 +0100

    Refactored unit tests for balance and top-up

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,15 +4,19 @@ describe Oystercard do
 
 	subject(:card) {described_class.new}
 
-	it {is_expected.to respond_to(:balance)}
+  describe "#balance" do
+    it {is_expected.to respond_to(:balance)}
 
-	it {is_expected.to respond_to(:top_up).with(1).argument }
+    it 'initializes Oystercard with default balance 0' do
+    expect(subject.balance).to eq 0
+    end
+  end
 
-	it 'initializes Oystercard with default balance 0' do
-		expect(subject.balance).to eq 0
-	end
+  describe "#top_up" do
+    it {is_expected.to respond_to(:top_up).with(1).argument }
 
-	it 'tops up with £10' do
-		expect(subject.top_up(10).balance).to eq 10
-	end
+  	it 'tops up with £10' do
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+  	end
+  end
 end
